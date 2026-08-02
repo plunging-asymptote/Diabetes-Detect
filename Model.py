76f8f7f620f0969027a87b4e13e1d680ace7030d
upsidedownai <karljohanekberg@gmail.com>
2026-08-02 11:41:15 +0200

    Update Model.py and PoolData.py
    Move code into main function.

--- Model.py
+++ Model.py
@@ -9,61 +9,64 @@ from sklearn.model_selection import cross_val_score
 import pandas
 import numpy
 
-DATA = pandas.read_csv("DATA.csv")
-
-DATA = pandas.DataFrame.dropna(DATA, axis = 0)
-DATA = DATA[DATA.BPXDI2 > 10]
-DATA = DATA[DATA.DIQ010 < 4]
-
-DATA = DATA.drop("URXUMA", axis = 1)
-
-DATA = DATA._get_numeric_data()
-DATA = DATA.replace({"DIQ010": 3}, 1)
-DATA = DATA.replace({"DIQ010": 2}, 0)
-
-numpy_array = DATA.to_numpy()
-
-pipe = make_pipeline(StandardScaler(), RandomForestClassifier(random_state = 0))
-
-X = numpy_array[:, 1:7]
-Y = numpy_array[:, 7]
-
-X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, random_state=0)
-
-
-pipe.fit(X_TRAIN, Y_TRAIN)
-param_grid = {
-   'randomforestclassifier__max_depth': [2, 4, 6, 8, 10],
-   'randomforestclassifier__n_estimators': [5, 10, 15, 20, 25]}
-
-search = GridSearchCV(pipe,
-                            param_grid,
-                            cv = 5)
-
-search.fit(X_TRAIN, Y_TRAIN)
-
 def cost_function(y, y_pred, neg_label, pos_label):
     cm = confusion_matrix(y, y_pred, labels = [neg_label, pos_label])
     cost_matrix = numpy.array([[0, -1], [-15, 0]])
     return numpy.sum(cm * cost_matrix)
 
-cost_scorer = make_scorer(cost_function, neg_label = 0, pos_label = 1)
+def main():
+    data = pandas.read_csv("data.csv")
+    
+    data = pandas.DataFrame.dropna(data, axis = 0)
+    data = data[data.BPXDI2 > 10]
+    data = data[data.DIQ010 < 4]
+    
+    data = data.drop("URXUMA", axis = 1)
+    
+    data = data._get_numeric_data()
+    data = data.replace({"DIQ010": 3}, 1)
+    data = data.replace({"DIQ010": 2}, 0)
+    
+    numpy_array = data.to_numpy()
+    
+    pipe = make_pipeline(StandardScaler(), RandomForestClassifier(random_state = 0))
+    
+    X = numpy_array[:, 1:7]
+    Y = numpy_array[:, 7]
+    
+    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, random_state=0)
+    
+    
+    pipe.fit(X_TRAIN, Y_TRAIN)
+    param_grid = {
+       'randomforestclassifier__max_depth': [2, 4, 6, 8, 10],
+       'randomforestclassifier__n_estimators': [5, 10, 15, 20, 25]}
+    
+    search = GridSearchCV(pipe,
+                                param_grid,
+                                cv = 5)
+    
+    search.fit(X_TRAIN, Y_TRAIN)
+    
+    cost_scorer = make_scorer(cost_function, neg_label = 0, pos_label = 1)
+    
+    tuned_model = TunedThresholdClassifierCV(
+        search,
+        scoring = cost_scorer,
+        store_cv_results = True)
+    tuned_model.fit(X_TRAIN, Y_TRAIN)
+            
+    print(cost_scorer(search, X_TEST, Y_TEST))
+    print(cost_scorer(tuned_model, X_TEST, Y_TEST))
+    
+    print(accuracy_score(tuned_model.predict(X_TEST), Y_TEST))
+    
+    testing = pandas.DataFrame((tuned_model.predict(X_TEST)))
+    
+    print(recall_score(Y_TEST, testing, average = "weighted"))
+    print(recall_score(Y_TEST, testing, pos_label = 0, average = "binary"))
+    scores = cross_val_score(tuned_model, X_TEST, Y_TEST, cv = 5)
+    print(scores.mean())
+    print(scores.std())
 
-tuned_model = TunedThresholdClassifierCV(
-    search,
-    scoring = cost_scorer,
-    store_cv_results = True)
-tuned_model.fit(X_TRAIN, Y_TRAIN)
-        
-print(cost_scorer(search, X_TEST, Y_TEST))
-print(cost_scorer(tuned_model, X_TEST, Y_TEST))
-
-print(accuracy_score(tuned_model.predict(X_TEST), Y_TEST))
-
-testing = pandas.DataFrame((tuned_model.predict(X_TEST)))
-
-print(recall_score(Y_TEST, testing, average = "weighted"))
-print(recall_score(Y_TEST, testing, pos_label = 0, average = "binary"))
-scores = cross_val_score(tuned_model, X_TEST, Y_TEST, cv = 5)
-print(scores.mean())
-print(scores.std())
+main()
--- PoolData.py
+++ PoolData.py
@@ -1,66 +1,69 @@
 import pandas
 import os
 
-DATA = pandas.DataFrame()
-ALB_DATA = pandas.DataFrame()
-BMX_DATA = pandas.DataFrame()
-BPX_DATA = pandas.DataFrame()
-DEMO_DATA = pandas.DataFrame()
-DIQ_DATA = pandas.DataFrame()
-GHB_DATA = pandas.DataFrame()
-
-#Read all data in folders containing datasets for each year of the surveys.
-for x in os.listdir(path = "NHANES_DATA/Pooled/ALB"):
-    ALB_DATASET = pandas.read_sas("NHANES_DATA/Pooled/ALB/" + x)
-    ALB_DATA = pandas.concat([ALB_DATA, ALB_DATASET[["SEQN", "URXUMA"]]])
+def main ():
+    data = pandas.DataFrame()
+    ALB_data = pandas.DataFrame()
+    BMX_data = pandas.DataFrame()
+    BPX_data = pandas.DataFrame()
+    DEMO_data = pandas.DataFrame()
+    DIQ_data = pandas.DataFrame()
+    GHB_data = pandas.DataFrame()
     
-for x in os.listdir(path = "NHANES_DATA/Pooled/BMX"):
-    BMX_DATASET = pandas.read_sas("NHANES_DATA/Pooled/BMX/" + x)
-    BMX_DATA = pandas.concat([BMX_DATA, BMX_DATASET[["SEQN", "BMXBMI"]]])
-
-for x in os.listdir(path = "NHANES_DATA/Pooled/BPX"):
-    BPX_DATASET = pandas.read_sas("NHANES_DATA/Pooled/BPX/" + x)
-    BPX_DATA = pandas.concat([BPX_DATA, BPX_DATASET[["SEQN", "BPXSY2", "BPXDI2"]]])
-    
-for x in os.listdir(path = "NHANES_DATA/Pooled/DEMO"):
-    DEMO_DATASET = pandas.read_sas("NHANES_DATA/Pooled/DEMO/" + x)
-    DEMO_DATA = pandas.concat([DEMO_DATA, DEMO_DATASET[["SEQN", "RIAGENDR", "RIDAGEYR"]]])
+    #Read all data in folders containing datasets for each year of the surveys.
+    for x in os.listdir(path = "NHANES_data/Pooled/ALB"):
+        ALB_dataSET = pandas.read_sas("NHANES_data/Pooled/ALB/" + x)
+        ALB_data = pandas.concat([ALB_data, ALB_dataSET[["SEQN", "URXUMA"]]])
         
-for x in os.listdir(path = "NHANES_DATA/Pooled/DIQ"):
-    DIQ_DATASET = pandas.read_sas("NHANES_DATA/Pooled/DIQ/" + x)
-    DIQ_DATA = pandas.concat([DIQ_DATA, DIQ_DATASET[["SEQN", "DIQ010"]]])
+    for x in os.listdir(path = "NHANES_data/Pooled/BMX"):
+        BMX_dataSET = pandas.read_sas("NHANES_data/Pooled/BMX/" + x)
+        BMX_data = pandas.concat([BMX_data, BMX_dataSET[["SEQN", "BMXBMI"]]])
     
-for x in os.listdir(path = "NHANES_DATA/Pooled/GHB"):
-    GHB_DATASET = pandas.read_sas("NHANES_DATA/Pooled/GHB/" + x)
-    GHB_DATA = pandas.concat([GHB_DATA, GHB_DATASET[["SEQN", "LBXGH"]]])
+    for x in os.listdir(path = "NHANES_data/Pooled/BPX"):
+        BPX_dataSET = pandas.read_sas("NHANES_data/Pooled/BPX/" + x)
+        BPX_data = pandas.concat([BPX_data, BPX_dataSET[["SEQN", "BPXSY2", "BPXDI2"]]])
+        
+    for x in os.listdir(path = "NHANES_data/Pooled/DEMO"):
+        DEMO_dataSET = pandas.read_sas("NHANES_data/Pooled/DEMO/" + x)
+        DEMO_data = pandas.concat([DEMO_data, DEMO_dataSET[["SEQN", "RIAGENDR", "RIDAGEYR"]]])
+            
+    for x in os.listdir(path = "NHANES_data/Pooled/DIQ"):
+        DIQ_dataSET = pandas.read_sas("NHANES_data/Pooled/DIQ/" + x)
+        DIQ_data = pandas.concat([DIQ_data, DIQ_dataSET[["SEQN", "DIQ010"]]])
+        
+    for x in os.listdir(path = "NHANES_data/Pooled/GHB"):
+        GHB_dataSET = pandas.read_sas("NHANES_data/Pooled/GHB/" + x)
+        GHB_data = pandas.concat([GHB_data, GHB_dataSET[["SEQN", "LBXGH"]]])
+    
+    #Ensure that only participants featured in all of the datasets are used.
+    ALB_data = ALB_data[ALB_data["SEQN"].isin(GHB_data["SEQN"]) == True]
+    BMX_data = BMX_data[BMX_data["SEQN"].isin(GHB_data["SEQN"]) == True]
+    BPX_data = BPX_data[BPX_data["SEQN"].isin(GHB_data["SEQN"]) == True]
+    DEMO_data = DEMO_data[DEMO_data["SEQN"].isin(GHB_data["SEQN"]) == True]
+    DIQ_data = DIQ_data[DIQ_data["SEQN"].isin(GHB_data["SEQN"]) == True]
+    
+    ALB_data = ALB_data.sort_values(by = ["SEQN"])
+    BMX_data = BMX_data.sort_values(by = ["SEQN"])
+    BPX_data = BPX_data.sort_values(by = ["SEQN"])
+    DEMO_data = DEMO_data.sort_values(by = ["SEQN"])
+    DIQ_data = DIQ_data.sort_values(by = ["SEQN"])
+    GHB_data = GHB_data.sort_values(by = ["SEQN"])
+    
+    #Reset index of all dataframes, ensuring that concating will work.
+    ALB_data = ALB_data.reset_index(drop = True)
+    BMX_data = BMX_data.reset_index(drop = True)
+    BPX_data = BPX_data.reset_index(drop = True)
+    DEMO_data = DEMO_data.reset_index(drop = True)
+    DIQ_data = DIQ_data.reset_index(drop = True)
+    GHB_data = GHB_data.reset_index(drop = True)
+    
+    #Concat datasets.
+    data = pandas.concat([GHB_data, ALB_data["URXUMA"]], axis = 1)
+    data = pandas.concat([data, BMX_data["BMXBMI"]], axis = 1)
+    data = pandas.concat([data, BPX_data[["BPXSY2", "BPXDI2"]]], axis = 1)
+    data = pandas.concat([data, DEMO_data[["RIAGENDR", "RIDAGEYR"]]], axis = 1)
+    data = pandas.concat([data, DIQ_data["DIQ010"]], axis = 1)
+    
+    data.to_csv("data.csv", index = False)
 
-#Ensure that only participants featured in all of the datasets are used.
-ALB_DATA = ALB_DATA[ALB_DATA["SEQN"].isin(GHB_DATA["SEQN"]) == True]
-BMX_DATA = BMX_DATA[BMX_DATA["SEQN"].isin(GHB_DATA["SEQN"]) == True]
-BPX_DATA = BPX_DATA[BPX_DATA["SEQN"].isin(GHB_DATA["SEQN"]) == True]
-DEMO_DATA = DEMO_DATA[DEMO_DATA["SEQN"].isin(GHB_DATA["SEQN"]) == True]
-DIQ_DATA = DIQ_DATA[DIQ_DATA["SEQN"].isin(GHB_DATA["SEQN"]) == True]
-
-ALB_DATA = ALB_DATA.sort_values(by = ["SEQN"])
-BMX_DATA = BMX_DATA.sort_values(by = ["SEQN"])
-BPX_DATA = BPX_DATA.sort_values(by = ["SEQN"])
-DEMO_DATA = DEMO_DATA.sort_values(by = ["SEQN"])
-DIQ_DATA = DIQ_DATA.sort_values(by = ["SEQN"])
-GHB_DATA = GHB_DATA.sort_values(by = ["SEQN"])
-
-#Reset index of all dataframes, ensuring that concating will work.
-ALB_DATA = ALB_DATA.reset_index(drop = True)
-BMX_DATA = BMX_DATA.reset_index(drop = True)
-BPX_DATA = BPX_DATA.reset_index(drop = True)
-DEMO_DATA = DEMO_DATA.reset_index(drop = True)
-DIQ_DATA = DIQ_DATA.reset_index(drop = True)
-GHB_DATA = GHB_DATA.reset_index(drop = True)
-
-#Concat datasets.
-DATA = pandas.concat([GHB_DATA, ALB_DATA["URXUMA"]], axis = 1)
-DATA = pandas.concat([DATA, BMX_DATA["BMXBMI"]], axis = 1)
-DATA = pandas.concat([DATA, BPX_DATA[["BPXSY2", "BPXDI2"]]], axis = 1)
-DATA = pandas.concat([DATA, DEMO_DATA[["RIAGENDR", "RIDAGEYR"]]], axis = 1)
-DATA = pandas.concat([DATA, DIQ_DATA["DIQ010"]], axis = 1)
-
-DATA.to_csv("DATA.csv", index = False)
+main()
